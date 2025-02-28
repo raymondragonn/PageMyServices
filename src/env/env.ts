889c531d21eedcd94785154fc172212ab9ea846e
Ray Mondragon <raymondragonn@gmail.com>
2025-02-27 19:28:22 -0600

    mejoras en estetica para dispositivos moviles

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,9 +1,9 @@
 import { provideCloudflareLoader } from "@angular/common";
 
 export const environment = {
-    //production: true,
-    //apiLocal: 'http://raymondragon.com/api/index.php'
+    production: true,
+    apiLocal: 'http://raymondragon.com/api/index.php'
 
-    production: false,
-    apiLocal: 'http://localhost/BackPageMyServices/index.php'
+    //production: false,
+    //apiLocal: 'http://localhost/BackPageMyServices/index.php'
   };
